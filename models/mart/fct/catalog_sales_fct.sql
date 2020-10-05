da39816b85872f66c3bb978226d247d4df7e3bdd
CloudChaoszero <rmaldonadocloud@outlook.com>
2020-10-04 22:47:49 -0700

    :art: Add Aggregation caculations

--- models/mart/fct/catalog_sales_fct.sql
+++ models/mart/fct/catalog_sales_fct.sql
@@ -3,6 +3,19 @@ WITH source AS (
 
     SELECT * FROM {{ ref('stg_customer_catalog_sales') }}
 
+),
+
+final AS (
+
+    SELECT 
+    
+         catalog_sales.*,
+        -- X $Dollars * (British Pounds / $Dollars)
+        net_amount * (.77/1) AS net_amount_british_pounds,
+        net_amount_british_pounds / quantity AS net_amount_per_order_unit_british_pounds
+    
+    FROM source AS catalog_sales
+
 )
 
-SELECT * FROM source
+SELECT * FROM final
